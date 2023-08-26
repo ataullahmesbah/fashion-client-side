c2771ffe3fedc27b173c3e44c1122cd30ef9d13b
ataullahmesbah <mesbahataullah4@gmail.com>
2023-08-26 19:57:24 +0600

    2nd pc code push

--- src/Pages/Home/Banner/Banner.jsx
+++ src/Pages/Home/Banner/Banner.jsx
@@ -1,54 +1,59 @@
-
-import banner from '../../../images/banner/img1.jpg';
-
+import AOS from "aos";
+import "aos/dist/aos.css";
+import { useEffect } from "react";
 
 
 const Banner = () => {
+
+    useEffect(() => {
+        AOS.init();
+    }, []);
+
+
+
     return (
-        <div className="relative h-[880px]  overflow-hidden bg-gradient-to-b from-blue-900 via-blue-800">
+
+        <div className="relative h-[950px]   bg-gradient-to-b from-blue-900 via-blue-800">
             {/* Banner Image Background */}
             <div
                 className="absolute inset-0 bg-cover bg-center  lg:pt-20 h-full"
                 style={{
-                    backgroundImage: `url(${banner})`,
+
+                    backgroundImage: `url('https://i.ibb.co/h9Mws60/image.png')`,
                 }}
             ></div>
 
             {/* Overlay */}
             <div
-                className="absolute inset-0 bg-gradient-to-b from-black via-blue-800 opacity-80 z-0"
+                className="absolute inset-0 bg-gradient-to-b from-black  opacity-90 z-0"
                 style={{
                     height: '90vh',
                 }}
             ></div>
 
             {/* Content */}
-            <div className="absolute inset-0 flex flex-col z-10 space-y-3 lg:space-y-28">
-                <div className="flex flex-col space-y-8 sm:flex-row sm:space-x-16 sm:items-start sm:justify-start lg:mt-1 lg:space-x-48 mt-40 sm:mt-28">
-                    {/* ... Your content ... */}
-                    <div className=" sm:text-left lg:px-36 px-3  lg:mt-48">
-                        <h6 className="text-3xl sm:text-5xl md:text-5xl md:px-10 text-white py-4 font-serif lg:mb-5">
-                            Make people fall in love with your clothes.
-                        </h6>
-                        <p className="uppercase font-mono bg-blue-900  md:mx-10 text-center rounded-xl text-white w-[300px] lg:mb-8">Order Now for the Latest Trends</p>
-                        <p className="uppercase font-mono  md:px-10 text-white py-4 ">Embrace style and grace with our exquisite collection of clothing. Discover the perfect blend of fashion <br /> and comfort that will make hearts swoon. Elevate your wardrobe with our curated selections, designed to <br /> captivate and inspire. Unleash your unique charm <br /> and confidence through our timeless fashion pieces.</p>
 
-                        <p className="text-3xl sm:text-5xl md:px-10 text-white py-4 font-mono">
-                            New Fashion Collection
-                        </p>
+            <div className="absolute inset-0 flex flex-col z-10 space-y-3 lg:space-y-28 ">
+                <div className="flex flex-col space-y-8 sm:flex-row sm:space-x-16 sm:items-start sm:justify-start lg:mt-1 lg:space-x-48 mt-40 mx-36 sm:mt-28 opacity-90">
+                    <div className="sm:text-left lg:px-36 px-3 lg:mt-48">
+                        <h6 data-aos="fade-right" data-aos-duration="2000" className="text-3xl sm:text-5xl md:text-7xl md:px-10 lg:font-extrabold text-gray-300 py-4 font-serif lg:mb-5">Make people fall in love with <br /> your clothes.</h6>
+                        <p data-aos="fade-left" data-aos-duration="3000" className="uppercase font-mono md:px-10 text-white py-4">Embrace style and grace with our exquisite collection of clothing. Discover the perfect blend of fashion <br /> and comfort that will make hearts swoon. Elevate your wardrobe with our curated selections, designed to <br /> captivate and inspire. Unleash your unique charm <br /> and confidence through our timeless fashion pieces.</p>
+                        <p data-aos="fade-right" data-aos-duration="2000" className="text-3xl sm:text-5xl md:px-10 text-white py-4 font-mono">New Fashion Collection</p>
+                        <div data-aos="fade-left" data-aos-duration="2000" className="">
+                        <button  className="btn btn-sm mx-10 mt-5 btn-secondary font-mono text-white font-semibold">Meet Tr-Fashion</button>
 
-                        <button className='btn btn-sm mx-10 mt-5 btn-secondary'>Learn More</button>
-                    </div>
-
-                    {/*            ------------------- */}
-
-
+                        </div>      
+                                      </div>
                 </div>
 
 
             </div>
+
         </div>
     );
 };
 
 export default Banner;
+
+
+
--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -14,3 +14,4 @@ const Home = () => {
 };
 
 export default Home;
+
